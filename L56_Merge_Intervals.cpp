e1bbf66b3b0f22d9b144065621b545b42382945b
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-04-06 22:15:59 +0530

    commit

--- L56_Merge_Intervals.cpp
+++ L56_Merge_Intervals.cpp
@@ -29,9 +29,37 @@ void print2Darr(vector<vector<int>> arr)
   }
 }
 
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+  vector<vector<int>> res;
+  int n = intervals.size(),start,end; 
+  sort(intervals.begin(),intervals.end());
+
+  cout<<"After sorting arr\n"; print2Darr(intervals);
+
+  for (int i = 0; i < n; i++)
+  {
+    if(res.empty() || intervals[i][0] > res.back()[1]){
+      res.push_back(intervals[i]);
+    }
+    else{
+      res.back()[1] = max(res.back()[1],intervals[i][1]);
+    }
+  }
+  return res;  
+}
+
 int main()
 {
   cout << "\n\n=======================\n\n";
+
+  // vector<vector<int>> arr = {{1, 3}, {2, 6}, {8, 10}, {12, 18}},;
+  // vector<vector<int>> arr = {{1,3},{4,5},{6,12},{7,8},{9,12},{13,14}};
+  // vector<vector<int>> arr = {{1,3},{4,5},{6,12},{7,8},{9,10},{12,14}};
+  vector<vector<int>> arr = {{1,2},{8,9},{2,4},{9,12},{2,6}};
+
+  vector<vector<int>> res = merge(arr);
+  cout<<"Result\n";
+  print2Darr(res);
 
   cout << "\n\n=======================";
   return 0;
