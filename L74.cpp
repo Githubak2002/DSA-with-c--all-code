8a536ff1ecf182751af493b18cd559dc3157f7df
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-04-07 22:42:17 +0530

    commit

--- L74.cpp
+++ L74.cpp
@@ -27,18 +27,16 @@ bool searchMatrix(vector<vector<int>> &matrix, int target)
     while (start <= end)
     {
         mid = start + (end - start) / 2;
-        row = mid / m;
+        row = mid / n;
         col = mid % n;
         if (matrix[row][col] == target)
         {
             return true;
         }
-        else if (target < matrix[row][col])
-        {
-            end = mid - 1;
-        }
-        else
+        else if (target > matrix[row][col])
             start = mid + 1;
+        else
+            end = mid - 1;
     }
     return false;
 }
@@ -79,24 +77,3 @@ int main()
 
     return 0;
 }
-
-/* OPTIMIZE CODE
-bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int left = 0, right = m * n - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int mid_val = matrix[mid / n][mid % n];
-
-            if (mid_val == target)
-                return true;
-            else if (mid_val < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
-        return false;
-    }
-*/
