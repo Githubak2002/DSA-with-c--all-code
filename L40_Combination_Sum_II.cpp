cef36fce5f15f7205bb11431516263c07701310f
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-05-16 12:03:41 +0530

    LL40. Combination Sum II - DSA

--- L40_Combination_Sum_II.cpp
+++ L40_Combination_Sum_II.cpp
@@ -30,29 +30,28 @@ void print2Darr(vector<vector<int>> arr){
 }
 
 
-void combiSum(int index,vector<vector<int>> &ans,int target,vector<int> candidates,vector<int> ds){
-
-  if(index == candidates.size()){
-    if(target == 0){
-      sort(ds.begin(),ds.end());
-      ans.push_back(ds);
-    }
+void combiSum(int index,vector<vector<int>> &ans,int target,vector<int> arr,vector<int> ds){
+  if(target == 0){
+    ans.push_back(ds);
     return;
   }
-
-  if(candidates[index]<=target){
-    ds.push_back(candidates[index]);
-    combiSum(index+1,ans,target-candidates[index],candidates,ds);
+  for (int i = index; i < arr.size(); i++)
+  {
+    if(i>index && arr[i] == arr[i-1]) continue;
+    if(arr[i] > target) break;
+    ds.push_back(arr[i]);
+    combiSum(i+1,ans,target-arr[i],arr,ds);
     ds.pop_back();
+
   }
-  combiSum(index+1,ans,target,candidates,ds);
   
 }
 
-vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
 {
   vector<vector<int>> ans;
   vector<int> ds;
+  sort(candidates.begin(),candidates.end());
   combiSum(0,ans,target,candidates,ds);
 
   return ans;
@@ -63,11 +62,12 @@ int main(){
   cout << "\n\n=======================\n\n";
   vector<int> q = {10,1,2,7,6,1,5};          // [ [1,1,6], [1,2,5], [1,7], [2,6] ]
   vector<int> q1 = {2,3,6,7};        
+  vector<int> q2 = {2,1,1,1,2};        
   int target = 8;
   vector<vector<int>> ans;
   vector<int> ds;
 
-  ans = combinationSum(q,target);
+  ans = combinationSum2(q,target);
   cout<<"All combinations are:"<<endl;
   print2Darr(ans);
 
